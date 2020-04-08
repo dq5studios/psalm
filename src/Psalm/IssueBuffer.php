889d52acddc78c36482c0d0cce7d7149f1fa2855
Ben Dusinberre <ben@dq5studios.com>
2020-04-07 20:17:12 -0500

    Add format check when creating output

--- src/Psalm/IssueBuffer.php
+++ src/Psalm/IssueBuffer.php
@@ -759,13 +759,8 @@ class IssueBuffer
                 $output = new GithubActionsReport($normalized_data, self::$fixable_issue_counts, $report_options);
                 break;
 
-            case Report::TYPE_PHP_STORM:
-                $output = new PhpStormReport($normalized_data, self::$fixable_issue_counts, $report_options);
-                break;
-
-            case Report::TYPE_SARIF:
-                $output = new SarifReport($normalized_data, self::$fixable_issue_counts, $report_options);
-                break;
+            default:
+                throw new \UnexpectedValueException('Unknown report format ' . $report_options->format);
         }
 
         return $output->create();
